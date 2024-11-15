e7d13226ad6874655799e9a97f6bbbaf68df0710
sp589 <128362227+sp589@users.noreply.github.com>
2024-11-15 21:03:23 +0530

    Update example1.py

--- test/example1.py
+++ test/example1.py
@@ -1,6 +1,7 @@
 import networkx as nx
+from project import module1 as lib1
 G=nx.karate_club_graph()
 graph=G.copy()
 #res=Leader_Based(graph,0.07)
-res=lib1.Community_dectection(graph,0,0.04)
+res=lib1.Community_dectection(graph,0,0.07)
 print(res)
