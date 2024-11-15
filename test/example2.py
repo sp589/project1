2a62a060a72683c4cb2ac5d296a5310797e1ed7e
sp589 <128362227+sp589@users.noreply.github.com>
2024-11-16 00:37:02 +0530

    Update example2.py

--- test/example2.py
+++ test/example2.py
@@ -1,1 +1,11 @@
-
+import zipfile
+import networkx as nx
+from Community_detection import Module1 as lbcd
+zf=zipfile.ZipFile("Dataset/football.zip")
+txt=zf.read('football.txt').decode()
+gml=zf.read('football.gml').decode()
+gml=gml.split('\n')[1:]
+G=nx.parse_gml(gml)
+graph=G.copy()
+res=lbcd.Community_dectection(graph,"California",0.05)
+print(res)
