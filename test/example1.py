7542b9807067536c9236b47fab2aed2af960cc18
sp589 <128362227+sp589@users.noreply.github.com>
2024-11-16 01:10:28 +0530

    Update example1.py

--- test/example1.py
+++ test/example1.py
@@ -1,5 +1,5 @@
 import networkx as nx
-from Community_detection import module1 as lbcd
+from src.Community_detection import module1 as lbcd
 G=nx.karate_club_graph()
 graph=G.copy()
 res=lbcd.Community_dectection(graph,0,0.07)
